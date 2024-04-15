6380393f32710aefa6ba84bb4ee36e4c9d4e3ffc
jamik-dev <jamik.oik.group@gmail.com>
2024-04-15 15:19:46 +0500

    adding brand add modal

--- pages/brands/index.vue
+++ pages/brands/index.vue
@@ -1,15 +1,63 @@
 <template>
   <main>
     <div class="p-6">
-      <button
+      <el-dialog title="Tips" top="50vh" custom-class="!bg-dark !rounded-xl !border !border-dark-3 -translate-y-1/2"
+        :visible.sync="brandModal" width="40%">
+        <div class="px-8">
+          <el-form :model="add" ref="dynamicValidateForm" class="demo-dynamic">
+            <el-form-item prop="brand"
+              :rules="[{ required: true, message: 'Please input brand name', trigger: 'blur' }]">
+              <p class="text-light font-semibold">Бренд <span class="text-blue">*</span></p>
+              <el-input v-model="add.brand" class="!w-full" placeholder="Зоговолок"></el-input>
+            </el-form-item>
+            <el-form-item prop="popular_brand">
+              <div class="flex items-center gap-4">
+                <el-switch :width="50" :inactive-color="'#0F1824'" :active-color="'#3B82F6'"
+                  v-model="add.popular_brand"></el-switch>
+                <p class="text-light font-semibold">Популярные бренды</p>
+              </div>
+            </el-form-item>
+            <el-form-item prop="url">
+              <el-upload ref="uploader" :limit="1" action="#" :on-change="handleUploader" list-type="picture-card"
+                :auto-upload="false">
+                <i class="el-icon-picture"></i>
+                <div slot="file" slot-scope="{file}">
+                  <img class="el-upload-list__item-thumbnail" :src="file.url" alt="brand img">
+                  <span class="el-upload-list__item-actions !border-dark-3">
+                    <span class="el-upload-list__item-preview" @click="handlePictureCardPreview(file)">
+                      <i class="el-icon-zoom-in"></i>
+                    </span>
+                    <span class="el-upload-list__item-delete" @click="handleRemove(file)">
+                      <i class="el-icon-delete"></i>
+                    </span>
+                  </span>
+                </div>
+              </el-upload>
+              <el-dialog custom-class="!bg-dark !rounded-xl !border !border-dark-3" width="50%" append-to-body
+                :visible.sync="addPicDialog">
+                <img width="100%" :src="add.url" alt="">
+              </el-dialog>
+            </el-form-item>
+            <div class="flex justify-end mt-4 pt-4 space-x-2 border-t border-dark-3">
+              <button @click="dialogVisible = false"
+                class="bg-dark-3 font-semibold rounded-lg py-2 px-4 text-light">Отмена</button>
+              <button @click="dialogVisible = false"
+                class="bg-blue font-semibold rounded-lg py-2 px-4 text-light hover:bg-blue-2 duration-200 flex items-center">
+                <i class="el-icon-circle-plus text-lg mr-1"></i>
+                Сохранить
+              </button>
+            </div>
+          </el-form>
+        </div>
+      </el-dialog>
+      <button @click="brandModal = true"
         class="py-2 px-4 ml-auto rounded-lg bg-blue hover:bg-blue-2 duration-200 mb-3 text-sm font-semibold flex items-center">
         <i class="el-icon-circle-plus text-lg mr-1"></i>
         Добавить бренд
       </button>
       <div class="rounded-xl bg-dark py-4 px-6 space-y-4 border border-dark-3">
         <div class="flex gap-2 justify-between">
-          <el-input v-model="search.brands" class="!w-2/3" placeholder="Поиск бренда"
-            prefix-icon="el-icon-search">
+          <el-input v-model="search.brands" class="!w-2/3" placeholder="Поиск бренда" prefix-icon="el-icon-search">
           </el-input>
           <button class="w-10 h-10 rounded-lg bg-blue hover:bg-blue-2 duration-200 text-2xl">
             <i class="el-icon-refresh"></i>
@@ -80,9 +128,57 @@ export default {
     return {
       search: {
         brands: ''
-      }
+      },
+      add: {
+        brand: '',
+        popular_brand: false,
+        url: ''
+      },
+      brandModal: false,
+      addPicDialog: false,
+    }
+  },
+  methods: {
+    handlePictureCardPreview(file) {
+      this.add.url = file.url;
+      this.addPicDialog = true;
+    },
+    handleRemove(file) {
+      this.$refs.uploader.handleRemove(file)
+      document.querySelector('.el-upload.el-upload--picture-card').style.display = 'inline-block';
+      this.add.url = '';
+    },
+    handleUploader(file) {
+      console.log(file);
+      document.querySelector('.el-upload.el-upload--picture-card').style.display = 'none';
     }
   }
 }
 </script>
-<style></style>
+<style>
+.el-switch__core {
+  height: 26px;
+  border-radius: 40px;
+}
+
+.el-switch__core:after {
+  width: 20px;
+  height: 20px;
+  top: 2px;
+  left: 2px;
+}
+
+.el-switch.is-checked .el-switch__core::after {
+  margin-left: -21px;
+}
+
+.el-upload--picture-card {
+  background-color: #0F1824;
+  border-color: #1C2E45;
+}
+
+.el-upload-list--picture-card .el-upload-list__item {
+  border-color: #1C2E45;
+  background-color: #0F1824;
+}
+</style>
