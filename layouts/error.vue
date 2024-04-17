ff9ff616ce32c1b74a362bb5e2fd61d0d41c69c5
jamik-dev <jamik.oik.group@gmail.com>
2024-04-17 19:38:02 +0500

    login skleton design done

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-screen h-screen flex items-center justify-center">
+    <h1 class="text-5xl font-bold text-red-500">NIGGA ERROR OCCURED! FIX THAT!</h1>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'empty'
+}
+</script>
+<style lang="">
+  
+</style>
